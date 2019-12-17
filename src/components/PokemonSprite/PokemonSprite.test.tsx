ee657c38ef8eae1da4e7ebde1e21edd52874369b
GuilhermeMoresco <guilhermemoresco@dbc-000729.dbccompany.com.br>
2019-12-17 11:24:54 -0300

    Pokemon Sprite component tests

--- src/components/PokemonSprite/PokemonSprite.test.tsx
+++ src/components/PokemonSprite/PokemonSprite.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
+import { expect as cexpect } from 'chai';
 
 import PokemonSprite from './PokemonSprite';
 
@@ -15,5 +17,35 @@ describe('The Pokemon Sprite component', () => {
     );
     const sprite = getByTestId('pokemonsprite');
     expect(sprite).toBeInTheDocument();
+  });
+
+  it('does not render upon error', () => {
+    const wrapper = mount(
+      <PokemonSprite
+        url={
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png'
+        }
+      />,
+    );
+
+    wrapper.simulate('error');
+
+    cexpect(wrapper.find('img')).not.to.have.length;
+  });
+
+  it('links to its represented pokemon', () => {
+    const history: Array<string> = [];
+    const wrapper = mount(
+      <PokemonSprite
+        url={
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png'
+        }
+        history={history}
+      />,
+    );
+
+    wrapper.simulate('click');
+
+    cexpect(history).to.have.length(1);
   });
 });
